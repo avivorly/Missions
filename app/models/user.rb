f64e77e0f4fdb079d4a80b45a26d7f0354d38ee5
paint <exit>
2014-06-21 22:25:35 +0300

    afterdevise2

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+
+  def self.current
+    Thread.current[:user]
+  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
