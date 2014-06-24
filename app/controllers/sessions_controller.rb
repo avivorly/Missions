fac19e4e4d9ce27fcb5122887f8e00713fd9b4c7
paint <exit>
2014-06-24 20:38:35 +0300

    ready to upload working with picture

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
 class SessionsController < ApplicationController
   skip_before_filter :identification
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
+    auth = env['omniauth.auth']
+    user = User.from_omniauth(auth)
+    session[:image]=  auth['info']['image']
     session[:user_id] = user.id
     redirect_to root_url
   end
