5e86592764f829c089cc0033eab07cd1bfee682c
Aviv <aviv.orly@netvision.net.il>
2014-09-17 19:20:55 +0300

    try to fix sql problem

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -4,8 +4,8 @@ class MissionsController < ApplicationController
   # GET /missions
   # GET /missions.json
   def index
-    @missions = if current_user.try(:id)
-                 current_user.missions.order(:done).all
+    @missions = if current_user.try(:id) && current_user.id.present?
+                  Mission.where(:user_id => current_user.id).order(:done).all
                else
                  [(Mission.new title: 'please load page again', id: -1)]
                end
