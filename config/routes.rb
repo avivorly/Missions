c9756374302ddf97233b529d952c221c0a2290c2
Aviv <aviv.orly@netvision.net.il>
2015-05-13 21:40:47 +0300

    index addad

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :ideas
+
   resources :missions
 
   devise_for :users
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
-  root 'missions#index'
+  root 'welcome#index'
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140924093504) do
+ActiveRecord::Schema.define(version: 20150513171640) do
 
   create_table "days", force: true do |t|
     t.date     "date"
     t.text     "brief"
     t.text     "grades"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ideas", force: true do |t|
+    t.string   "title"
+    t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
