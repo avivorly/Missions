91a20e759ba28b636ca0f35e2fd81e8e276fcc2d
Aviv <aviv.orly@netvision.net.il>
2015-06-13 20:19:42 +0300

    working!!!!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def identification # TODO
     # only for home use
-   # @current_user = User.first
+
    # session[:user_id] = true
     redirect_to log_in_path unless session[:user_id]
   end
